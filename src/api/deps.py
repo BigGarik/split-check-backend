2eeef6d6df7ccf226ed1ede0c7941c9e0679c721
Игорь <inetsmol@gmail.com>
2025-01-21 15:00:08 +0300

    рабочая проверка пользователя через firebase

--- src/api/deps.py
+++ src/api/deps.py
@@ -15,32 +15,32 @@ from src.repositories.user import get_user_by_email
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/token")
 
 
-async def get_current_user(
-        request: Request,
-        token: Optional[str] = Depends(oauth2_scheme)
-):
+async def get_current_user_local(request: Request, token: str = Depends(oauth2_scheme)):
+    email, _ = await verify_token(settings.access_secret_key, token=token)
+    return email
+
+
+async def get_current_user(request: Request):
     """
     Dependency для проверки и получения текущего пользователя через Firebase или OAuth2.
     """
     try:
         # Попытка получить токен из заголовка Authorization
-        id_token = request.headers.get('Authorization')
-
-        if token:
-            # Обработка OAuth2 токена
-            email, _ = await verify_token(settings.access_secret_key, token=token)
-
-        elif id_token:
+        token = request.headers.get('Authorization')
+        if not token:
+            raise 401
+        if request.headers.get('firebase_token'):
             # Обработка Firebase токена
-            claims = await get_token_from_redis(id_token)
+            claims = await get_token_from_redis(token)
             logger.debug(f"claims: {claims}")
             if not claims:
-                logger.debug(f"token: {id_token}")
-                claims = get_firebase_user(id_token)
-                await add_token_to_redis(id_token, claims)
+                logger.debug(f"token: {token}")
+                claims = get_firebase_user(token)
+                await add_token_to_redis(token, claims)
             email = claims.get('email')
+
         else:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token not provided")
+            email = await get_current_user_local(request)
 
         # Получение пользователя по email
         user = await get_user_by_email(email)
