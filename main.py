03f0256d0c5b46c6f8095599fabcd1d2eb0311a9
Demerchyan Eduard <edemerchan@yandex.ru>
2025-02-17 12:23:58 +0000

    main.py edited online with Bitbucket

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+from redis import asyncio as aioredis
 
 app = FastAPI()
 
+redis_client = aioredis.from_url("redis://192.168.0.4", encoding="utf8", decode_responses=True)
+
+
 @app.get("/")
-async def root():
+async def root():    
     return {"message": "Привет, мир!"}
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
-    return {"item_id": item_id}
+    await redis_client.set("test", item_id)
+    return {"item_id": redis_client.get("test")}
