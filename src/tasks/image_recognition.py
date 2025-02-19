545ca202d8f1ab41256c7d8e7707a36d30f201ca
Игорь <inetsmol@gmail.com>
2025-02-19 13:44:59 +0300

    recognized_json через нейронку в проде

--- src/tasks/image_recognition.py
+++ src/tasks/image_recognition.py
@@ -31,83 +31,85 @@ async def recognize_image_task(
         classification_result = await classifier_image(image_path)
         if classification_result == "Allowed Content":
             # Распознавание чека
-            # recognized_json = await recognize_check_by_anthropic(file_location_directory)
-            recognized_json = {
-                "restaurant": "Bistro",
-                "table_number": "1",
-                "order_number": "998",
-                "date": "19.11.2024",
-                "time": "13:26",
-                "waiter": "Kacca 2",
-                "items": [
-                    {
-                        "id": 1,
-                        "name": "Бабушкин хлеб",
-                        "quantity": 2,
-                        "price": 2000,
-                        "sum": 4000
+            if settings.environment == "prod":
+                recognized_json = await recognize_check_by_anthropic(file_location_directory)
+            else:
+                recognized_json = {
+                    "restaurant": "Bistro",
+                    "table_number": "1",
+                    "order_number": "998",
+                    "date": "19.11.2024",
+                    "time": "13:26",
+                    "waiter": "Kacca 2",
+                    "items": [
+                        {
+                            "id": 1,
+                            "name": "Бабушкин хлеб",
+                            "quantity": 2,
+                            "price": 2000,
+                            "sum": 4000
+                        },
+                        {
+                            "id": 2,
+                            "name": "Солянка",
+                            "quantity": 1,
+                            "price": 16000,
+                            "sum": 16000
+                        },
+                        {
+                            "id": 3,
+                            "name": "Куриный суп с лапшой",
+                            "quantity": 1,
+                            "price": 16000,
+                            "sum": 16000
+                        },
+                        {
+                            "id": 4,
+                            "name": "Говядина с грибами",
+                            "quantity": 1,
+                            "price": 21000,
+                            "sum": 21000
+                        },
+                        {
+                            "id": 5,
+                            "name": "Рис отварной",
+                            "quantity": 0.5,
+                            "price": 7000,
+                            "sum": 3500
+                        },
+                        {
+                            "id": 6,
+                            "name": "Гречка",
+                            "quantity": 0.5,
+                            "price": 6000,
+                            "sum": 3000
+                        },
+                        {
+                            "id": 7,
+                            "name": "Ёжик из курицы",
+                            "quantity": 1,
+                            "price": 12000,
+                            "sum": 12000
+                        },
+                        {
+                            "id": 8,
+                            "name": "Сендвич с курицей",
+                            "quantity": 1,
+                            "price": 13000,
+                            "sum": 13000
+                        }
+                    ],
+                    "subtotal": 88500,
+                    "service_charge": {
+                        "name": "",
+                        "amount": 0
                     },
-                    {
-                        "id": 2,
-                        "name": "Солянка",
-                        "quantity": 1,
-                        "price": 16000,
-                        "sum": 16000
+                    "vat": {
+                        "rate": 0,
+                        "amount": 0
                     },
-                    {
-                        "id": 3,
-                        "name": "Куриный суп с лапшой",
-                        "quantity": 1,
-                        "price": 16000,
-                        "sum": 16000
-                    },
-                    {
-                        "id": 4,
-                        "name": "Говядина с грибами",
-                        "quantity": 1,
-                        "price": 21000,
-                        "sum": 21000
-                    },
-                    {
-                        "id": 5,
-                        "name": "Рис отварной",
-                        "quantity": 0.5,
-                        "price": 7000,
-                        "sum": 3500
-                    },
-                    {
-                        "id": 6,
-                        "name": "Гречка",
-                        "quantity": 0.5,
-                        "price": 6000,
-                        "sum": 3000
-                    },
-                    {
-                        "id": 7,
-                        "name": "Ёжик из курицы",
-                        "quantity": 1,
-                        "price": 12000,
-                        "sum": 12000
-                    },
-                    {
-                        "id": 8,
-                        "name": "Сендвич с курицей",
-                        "quantity": 1,
-                        "price": 13000,
-                        "sum": 13000
-                    }
-                ],
-                "subtotal": 88500,
-                "service_charge": {
-                    "name": "",
-                    "amount": 0
-                },
-                "vat": {
-                    "rate": 0,
-                    "amount": 0
-                },
-                "total": 88500
-            }
+                    "total": 88500
+                }
 
             logger.debug(f"Recognition completed for check_uuid {check_uuid}")
 
